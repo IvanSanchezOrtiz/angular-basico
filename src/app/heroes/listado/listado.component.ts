cfdfb824dc36e5444ef4c3ddd80dd2d0589c4d19
Ivan Sanchez <i.soivan92@outlook.com>
2022-10-05 19:29:08 -0500

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent  {
+
+  heroes: string[] = ['Spiderman','Ironman','Thor','Hulk'];
+  heroeBorrado = ''; 
+
+  borrarHeroe(){
+   this.heroeBorrado = this.heroes.shift() || '';
+  }
+
+
+}
